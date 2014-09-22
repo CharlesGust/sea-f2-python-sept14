35f450b90f1ee2b8d92e5ec8a694b3b22c763486
Charles Gust <charlesg@equality.net>
2014-09-22 16:57:10 -0700

    factor out individual functions, generate report

--- Students/CharlesGust/session03/mailroom.py
+++ Students/CharlesGust/session03/mailroom.py
@@ -1,40 +1,92 @@
-donation_db = [["Alice", [500, 100, 100]],
+#!/usr/bin/python
+
+donation_DB = [["Alice", [500, 100, 100]],
                ["Betty", [50]],
                ["Carol", [1000, 10, 10]],
                ["Debbie", [25, 25, 25]],
                ["Ellie", [200, 200]]]
 
 
-def send_Thank_You():
-    full_name = raw_input("What is the Full Name?")
-    if full_name == "list":
-        for donor in donation_db:
-            print donor[0]
-    elif full_name not in donation_db:
-        donation_db += [full_name]
-    ask_amount = True
-    while ask_amount:
-        amount = raw_input("What is the donation amount?")
-        if type(amount) == float or type(amount) == int:
-            ask_amount = False
+def search_DB(full_name):
+    """ search donation database, return None if name is not found """
+    global donation_DB
+
+    for donor in donation_DB:
+        if donor[0] == full_name:
+            return donor
+    return None
+
+
+def ask_Name():
+    """ prompt the user for a name, or request for a list """
+    global donation_DB
+
+    while True:
+        full_name = raw_input("What is the Full Name?")
+        if full_name == "list":
+            for donor in donation_DB:
+                print donor[0]
         else:
+            if not search_DB(full_name):
+                donation_DB.append([full_name, []])
+            return full_name
+
+
+def ask_Amount():
+    """ prompt the user for a donation amount """
+    while True:
+        amount = input("What is the donation amount?")
+        if type(amount) != float and type(amount) != int:
             print "Please enter a number"
-    donation_db[full_name][1] += amount
+        else:
+            return amount
+
+
+def record_Donation(full_name, amount):
+    global donation_db
+
+    for donor in donation_DB:
+        if donor[0] == full_name:
+            donor[1].append(amount)
+            return
+    else:
+        """ if name previously inserted when not found, name will be found """
+        assert(False)
+        donation_DB.append([full_name, [amount]])
+
+
+def send_ThankYou():
+    """ thanks donors for their gifts """
+
+    full_name = ask_Name()
+    amount = ask_Amount()
+    record_Donation(full_name, amount)
+
     print "Dear %(donor)s, Thank you very much for your recent donation\
-    of %(amount)i" % {"donor": full_name, "amount": amount}
+ of $%(amount).2f" % {"donor": full_name, "amount": amount}
+    return
 
 
-def create_report():
+def create_Report():
+    """ display formatted table of donation database """
+
+    print "{0:20s} {1:5s} {2:7s}".format("Name", "Total", "Average")
+    for donor in donation_DB:
+        donor_name = donor[0]
+        if len(donor[0]) > 20:
+            donor_name = donor_name[0:17]+"..."
+        print "{0:20s} {1:5d} {2:7d}".format(donor_name,
+                                             sum(donor[1]),
+                                             sum(donor[1])/len(donor[1]))
     pass
 
 if __name__ == "__main__":
-    ask_action = True
-    while ask_action:
+    while True:
         action = raw_input("Choose: 1. Send a Thank You, 2. Create a Report: ")
 
         if int(action) == 1:
-            send_Thank_You()
+            send_ThankYou()
         elif int(action) == 2:
-            create_report()
+            create_Report()
         else:
-            ask_action = False
+            break
