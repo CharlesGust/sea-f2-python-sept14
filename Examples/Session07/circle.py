8641a9b6b1622db6a9f7aaaf9323f25750cb6ee7
Charles Gust <charlesg@equality.net>
2014-09-29 22:21:58 -0700

    add property for radius, diameter and area; plus class method

--- Examples/Session07/circle.py
+++ Examples/Session07/circle.py
@@ -7,4 +7,35 @@ import math
 
 
 class Circle(object):
-    pass
+    def __init__(self, radius):
+        self.radius = radius
+        # self.diameter = radius * 2
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        self._radius = value
+
+    @property
+    def diameter(self):
+        return self._radius * 2
+
+    @diameter.setter
+    def diameter(self, value):
+        self._radius = value / 2
+
+    @property
+    def area(self):
+        return self._radius ** 2 * math.pi
+
+    @area.setter
+    def area(self, value):
+        raise AttributeError
+        self._radius = math.sqrt(value / math.pi)
+
+    @classmethod
+    def from_diameter(cls, diameter):
+        return cls(diameter/2)
